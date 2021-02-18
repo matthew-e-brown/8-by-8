3248e72e46c390506a27e5bfec59eaff86e3c045
Matthew Brown <matthew.e.brown.17@gmail.com>
2021-02-18 18:45:41 -0500

    v1.2.2
    - Refactor JS code
      - under 80 chars
      - clearer name

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const blankArray = () => Array(8).fill(undefined).map(_ => Array(8).fill(undefined).map(_ => false));
+const reset = () => Array(8).fill().map(() => Array(8).fill().map(() => false));
 
 const App = Vue.createApp({
   data: function() {
-    return { bits: blankArray() };
+    return { bits: reset() };
   },
   methods: {
     toggle: function(i, j) {
       this.bits[i][j] = !this.bits[i][j];
     },
     rowSum: function(i) {
-      return this.bits[i].reduce((sum, acc, j) => sum + (acc ? 2 ** (7 - j) : 0), 0);
+      return this.bits[i]
+        .reduce((sum, bit, j) => sum + (bit ? 2 ** (7 - j) : 0), 0);
     },
     clear: function() {
-      this.bits = blankArray();
+      this.bits = reset();
     }
   },
   computed: {
     binary: function() {
-      return this.bits.map(row => row.map(bit => bit ? '1' : '0').join('')).join('\n');
+      return this.bits
+        .map(row => row.map(bit => bit ? '1' : '0').join(''))
+        .join('\n');
     },
     decimal: function() {
       return this.bits.map((_, i) => this.rowSum(i)).join(', ');
